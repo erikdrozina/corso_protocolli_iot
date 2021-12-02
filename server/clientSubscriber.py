a056d21f45b040c1b805d7c96472b590dfe8eefa
Erik Drozina <erikdro41@gmail.com>
2021-12-02 17:33:32 +0100

    server: Add sub that receive sensors data
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

---
+++ server/clientSubscriber.py
@@ -0,0 +1,17 @@
+from paho.mqtt.client import Client
+
+client = Client(client_id="controlCenter")
+client.connect("192.168.104.150")
+
+# name of the topic the client subscribe to
+client.subscribe('iot/drone/1/sensors', qos=1)
+
+def on_message(client, userdata,message):
+    print(f"Message received from topic {message.topic}: " ,str(message.payload.decode("utf-8")))
+
+
+def run():
+    client.on_message=on_message
+    client.loop_forever()
+
+run()
